0782db5292bcaa6f149b0aa6282e767dae233cc5
hulmgulm <hulmgulm@users.noreply.github.com>
2024-10-22 15:53:00 +0200

    Update: Build without PWA

---
+++ vite-nopwa.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "",
+  build: {
+    assetsDir: "resources",
+  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./node_modules", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+});
